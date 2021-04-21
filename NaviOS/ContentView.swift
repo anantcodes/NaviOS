3e255e03aed1bea210e7e48c2588962f30f56eba
Anant Kanchan <anant4265@gmail.com>
2021-04-21 19:54:41 +0530

    initial commit

---
+++ NaviOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NaviOS
+//
+//  Created by Anant Kanchan on 21/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NaviOS/NaviOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaviOSApp.swift
+//  NaviOS
+//
+//  Created by Anant Kanchan on 21/04/21.
+//
+
+import SwiftUI
+
+@main
+struct NaviOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
